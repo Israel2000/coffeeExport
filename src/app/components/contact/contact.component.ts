a6be879ea3d070d487835ad5dd280dbf4add76bd
Israel Belete <ibelete2000@gmail.com>
2025-05-19 16:36:50 -0600

    email backend contact added

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from '../navbar/navbar.component';
-// footer component
 import { FooterComponent } from '../footer/footer.component';
+import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
 
 @Component({
   selector: 'app-contact',
@@ -11,5 +11,28 @@ import { FooterComponent } from '../footer/footer.component';
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  public sendEmail(e: Event) {
+    e.preventDefault();
 
+    emailjs
+      .sendForm(
+        'service_yckfuua',      //emailjs service ID
+        'template_zz31dm2',     //emailjs template ID
+        e.target as HTMLFormElement,
+        {
+          publicKey: 'QyE6mWSbbbvjCixad',  //emailjs public key
+        }
+      )
+      .then(
+        () => {
+          console.log('SUCCESS!');
+          alert('Your message has been sent!');
+          (e.target as HTMLFormElement).reset();
+        },
+        (error: EmailJSResponseStatus) => {
+          console.log('FAILED...', error.text);
+          alert('Failed to send message. Please try again later.');
+        }
+      );
+  }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -9,10 +9,10 @@ export class ProductService {
   private products: Product[] = [
     {
       id: 1,
-      name: "Espresso",
+      name: "Sedamo",
       price: 7.49,
       description: "Espresso is a strong, rich coffee made by forcing hot water through finely-ground coffee beans.",
-      image: "https://images.unsplash.com/photo-1510707577719-ae7c14805e3a?w=400",
+      image: 'assets/images/coffee_bag.jpg', 
       rating: 4.7,
       reviews: 105
     },
@@ -34,42 +34,42 @@ export class ProductService {
       rating: 4.7,
       reviews: 105
     },
-    {
-      id: 4,
-      name: "Americano",
-      price: 4.49,
-      description: "Americano is a creamy coffee drink made with espresso and steamed milk.",
-      image: "https://images.unsplash.com/photo-1521302080334-4bebac2763a6?w=400",
-      rating: 4.7,
-      reviews: 105
-    },
-    {
-      id: 5,
-      name: "Mocha",
-      price: 4.49,
-      description: "Mocha is a strong, rich coffee made by forcing hot water through finely-ground coffee beans.",
-      image: "https://images.unsplash.com/photo-1570968915860-54d5c301fa9f?w=400",
-      rating: 4.7,
-      reviews: 105
-    },
-    {
-      id: 6,
-      name: "Macchiato",
-      price: 4.49,
-      description: "Macchiato is a creamy coffee drink made with espresso and steamed milk.",
-      image: "https://images.unsplash.com/photo-1485808191679-5f86510681a2?w=400",
-      rating: 4.7,
-      reviews: 105
-    },
-    {
-      id: 7,
-      name: "Flat White",
-      price: 4.49,
-      description: "Flat White is a creamy coffee drink made with espresso and steamed milk.",
-      image: "https://images.unsplash.com/photo-1517701604599-bb29b565090c?w=400",
-      rating: 4.7,
-      reviews: 105
-    }
+    // {
+    //   id: 4,
+    //   name: "Americano",
+    //   price: 4.49,
+    //   description: "Americano is a creamy coffee drink made with espresso and steamed milk.",
+    //   image: "https://images.unsplash.com/photo-1521302080334-4bebac2763a6?w=400",
+    //   rating: 4.7,
+    //   reviews: 105
+    // },
+    // {
+    //   id: 5,
+    //   name: "Mocha",
+    //   price: 4.49,
+    //   description: "Mocha is a strong, rich coffee made by forcing hot water through finely-ground coffee beans.",
+    //   image: "https://images.unsplash.com/photo-1570968915860-54d5c301fa9f?w=400",
+    //   rating: 4.7,
+    //   reviews: 105
+    // },
+    // {
+    //   id: 6,
+    //   name: "Macchiato",
+    //   price: 4.49,
+    //   description: "Macchiato is a creamy coffee drink made with espresso and steamed milk.",
+    //   image: "https://images.unsplash.com/photo-1485808191679-5f86510681a2?w=400",
+    //   rating: 4.7,
+    //   reviews: 105
+    // },
+    // {
+    //   id: 7,
+    //   name: "Flat White",
+    //   price: 4.49,
+    //   description: "Flat White is a creamy coffee drink made with espresso and steamed milk.",
+    //   image: "https://images.unsplash.com/photo-1517701604599-bb29b565090c?w=400",
+    //   rating: 4.7,
+    //   reviews: 105
+    // }
   ];
 
   constructor() {}
